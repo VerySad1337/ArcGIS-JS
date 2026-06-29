5cb89804943b1321f7ec6dedc4332ce158d6044b
Eric Tan <116254341+VerySad1337@users.noreply.github.com>
2026-06-29 16:07:50 +0800

    Working version

--- my-arcgis-app/src/components/RouteMap.jsx
+++ my-arcgis-app/src/components/RouteMap.jsx
@@ -1,64 +1,23 @@
-import { useEffect, useRef } from "react";
-
 import "@arcgis/map-components/components/arcgis-map";
 import "@arcgis/map-components/components/arcgis-scene";
 import "@arcgis/map-components/components/arcgis-zoom";
 
-import Graphic from "@arcgis/core/Graphic";
-import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
-
 export default function RouteMap({
-  is3D,
-  webMapId,
-  webSceneId,
-  routeGeometry
+is3D,
+webMapId,
+webSceneId
 }) {
-  const graphicsLayerRef = useRef(new GraphicsLayer());
+if (!is3D) {
+return ( <arcgis-map
+     item-id={webMapId}
+     class="map-view"
+   > <arcgis-zoom slot="top-left"></arcgis-zoom> </arcgis-map>
+);
+}
 
-  const handleViewReady = async (event) => {
-    const view = event.target.view;
-
-    if (!view.map.layers.includes(graphicsLayerRef.current)) {
-      view.map.add(graphicsLayerRef.current);
-    }
-  };
-
-  useEffect(() => {
-    if (!routeGeometry) return;
-
-    graphicsLayerRef.current.removeAll();
-
-    const routeGraphic = new Graphic({
-      geometry: routeGeometry,
-      symbol: {
-        type: "simple-line",
-        color: [0, 0, 255],
-        width: 4
-      }
-    });
-
-    graphicsLayerRef.current.add(routeGraphic);
-  }, [routeGeometry]);
-
-  if (!is3D) {
-    return (
-      <arcgis-map
-        item-id={webMapId}
-        class="map-view"
-        onarcgisViewReadyChange={handleViewReady}
-      >
-        <arcgis-zoom slot="top-left"></arcgis-zoom>
-      </arcgis-map>
-    );
-  }
-
-  return (
-    <arcgis-scene
-      item-id={webSceneId}
-      class="scene-view"
-      onarcgisViewReadyChange={handleViewReady}
-    >
-      <arcgis-zoom slot="top-left"></arcgis-zoom>
-    </arcgis-scene>
-  );
+return ( <arcgis-scene
+   item-id={webSceneId}
+   class="scene-view"
+ > <arcgis-zoom slot="top-left"></arcgis-zoom> </arcgis-scene>
+);
 }
