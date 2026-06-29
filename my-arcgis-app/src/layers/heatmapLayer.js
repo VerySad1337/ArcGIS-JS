2d268fd80283b9997ca496a0962f385aa6d7c1fe
Eric Tan <116254341+VerySad1337@users.noreply.github.com>
2026-06-29 19:03:26 +0800

    Implemented heatmap, toggle on off for routing and intensity bar for heatmap

---
+++ my-arcgis-app/src/layers/heatmapLayer.js
@@ -0,0 +1,23 @@
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+
+export function createHeatmapLayer(url) {
+  return new FeatureLayer({
+    url,
+
+    // IMPORTANT: prevent portal metadata auth fallback
+    outFields: ["*"],
+
+    renderer: {
+      type: "heatmap",
+      colorStops: [
+        { ratio: 0, color: "rgba(0,0,255,0)" },
+        { ratio: 0.3, color: "blue" },
+        { ratio: 0.5, color: "cyan" },
+        { ratio: 0.7, color: "yellow" },
+        { ratio: 1, color: "red" }
+      ],
+      maxPixelIntensity: 50,
+      minPixelIntensity: 0
+    }
+  });
+}
